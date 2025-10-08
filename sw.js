3e20205f2a0ae59b79d0e3f489ee72a37f459380
adriancastroleiva <adrian.castroleiva@asturias.org>
2025-10-08 11:39:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,12 @@
-const CACHE_NAME='sepa-cache-v1';
-const ASSETS=[
-  './', './index.html', './style.css', './app.js', './config.js', './manifest.webmanifest',
-  './assets/icons/icon-192.png', './assets/icons/icon-512.png'
-];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME && caches.delete(k)))));
-});
-self.addEventListener('fetch', e=>{
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
-    e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));
+const CACHE='sepa-pwa-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(url.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+      const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;
+    })));
   }
 });
