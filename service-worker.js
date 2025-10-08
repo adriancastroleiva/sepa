d6e4716aa3860a6d78ee90b1c56c6032cd9a8b4d
adriancastroleiva <adrian.castroleiva@asturias.org>
2025-10-08 19:16:18 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -20,3 +20,33 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+const CACHE = 'sepa-cache-v5';  // ¡sube versión en cada deploy!
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const c = await caches.open(CACHE);
+    await c.addAll([
+      './',
+      './index.html',
+      './app.js',
+      './db.js',
+      './manifest.webmanifest',
+      './assets/icons/icon-192.png',
+      './assets/icons/icon-512.png',
+      './tester.html'
+    ]);
+    self.skipWaiting(); // <-- entra en "activated" sin esperar
+  })());
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null));
+    await self.clients.claim(); // <-- toma control de todas las pestañas
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
